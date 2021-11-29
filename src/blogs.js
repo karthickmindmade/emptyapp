0c3336db52c2ace0fa013c0275ed1aed98374843
admin <karthickdurai587@gmail.com>
2021-11-29 13:22:55 +0530

    uio78o

--- src/blogs.js
+++ src/blogs.js
@@ -10,32 +10,32 @@ function Blog() {
 
     return (
         <div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ms-5">
-      <li class="nav-item">
-        <Link class="nav-link active" to="/">home</Link>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav ms-5">
+      <li className="nav-item">
+        <Link className="nav-link active" to="/">home</Link>
         </li>
-        <li class="nav-item">
-        <Link class="nav-link active" to="/about">About</Link>
+        <li className="nav-item">
+        <Link className="nav-link active" to="/about">About</Link>
         </li>
-        <li class="nav-item">
-        <Link class="nav-link active" to="/blogs">blogs</Link>
+        <li className="nav-item">
+        <Link className="nav-link active" to="/blogs">blogs</Link>
         </li>
      
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown link
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
       </ul>
