ff74ab81efd2a0f6eb3e9110803325b4241a273d
admin <karthickdurai587@gmail.com>
2021-11-29 13:30:45 +0530

    dfhtdfg

--- src/blogs.js
+++ src/blogs.js
@@ -12,7 +12,7 @@ function Blog() {
         <div>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" >Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -29,13 +29,13 @@ function Blog() {
         </li>
      
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown link
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Another action</a></li>
+            <li><a className="dropdown-item" >Something else here</a></li>
           </ul>
         </li>
       </ul>
