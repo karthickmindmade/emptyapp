293480526edac5010cd0adf2ce48d27b4f4ba077
karthick587 <85030277+karthick587@users.noreply.github.com>
2021-12-21 02:10:15 +0530

    h

--- src/products.js
+++ src/products.js
@@ -55,7 +55,7 @@ var[productsList,setProductsList] = useState([]);
                            ){
                                return val;
                            }
-                       }).map((productsList)=>
+                       }) return.map((productsList)=>
 
 <ProductLayout
   productimg={<img src={productsList.image}  alt="avatar"/>}
