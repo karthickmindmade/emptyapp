c3024f7a974dcea94087794b3c60c73d1ec6b9f1
admin <karthickdurai587@gmail.com>
2022-02-04 16:25:38 +0530

    all

--- src/contex/productprovider.js
+++ src/contex/productprovider.js
@@ -1,9 +1,17 @@
 import { createContext, useState, useEffect } from "react";
 export const CounterContext = createContext();
 export default function CounterContextProvider(props) {
-    const [value, setValue] = useState([{"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}}]);
+    const [value, setValue] = useState([{"id":1,
+    "imgurl":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price":109.95,
+    "count": 1
+}]);
     useEffect(()=>{
-      setValue(JSON.parse(window.localStorage.getItem("storevalue")))
+        if(JSON.parse(window.localStorage.getItem("storevalue"))!==null){
+            setValue(JSON.parse(window.localStorage.getItem("storevalue")))
+        }
+     
     },[setValue])
     const [filtervalue, setfiltervalue] = useState('')
     //fuction to add product into cart
